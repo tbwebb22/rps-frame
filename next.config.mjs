701d93d257de21ffc1a5889581ae396375ef93d7
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2024-03-24 15:29:48 +0700

    add routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*",
+        protocol: "http",
+      },
+      {
+        hostname: "*",
+        protocol: "https",
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@resvg/resvg-js"],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      sharp: "commonjs sharp",
+      "@resvg/resvg-js": "commonjs @resvg/resvg-js",
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
